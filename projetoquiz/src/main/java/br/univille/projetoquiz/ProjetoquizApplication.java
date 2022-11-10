f4b5f2f364635155d849503c224f246ea5f54dfc
Binelo <jppedrobinelo@gmail.com>
2022-11-10 14:18:31 -0300

    first commit

---
+++ projetoquiz/src/main/java/br/univille/projetoquiz/ProjetoquizApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetoquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoquizApplication.class, args);
+	}
+
+}
---
+++ projetoquiz/src/test/java/br/univille/projetoquiz/ProjetoquizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetoquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
