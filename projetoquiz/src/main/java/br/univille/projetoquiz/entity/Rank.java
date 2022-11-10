81fe21ae36a271f15fd2c0fc9cbf318de4826e52
Binelo <jppedrobinelo@gmail.com>
2022-11-10 16:40:53 -0300

    rankeservice

--- projetoquiz/src/main/java/br/univille/projetoquiz/entity/Rank.java
+++ projetoquiz/src/main/java/br/univille/projetoquiz/entity/Rank.java
@@ -1,10 +1,15 @@
 package br.univille.projetoquiz.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Rank {
     private int pontuacao;
+    @ManyToMany
+    private Usuario ranque;
+
+    
 
     public int getPontuacao() {
         return pontuacao;
@@ -12,6 +17,14 @@ public class Rank {
 
     public void setPontuacao(int pontuacao) {
         this.pontuacao = pontuacao;
+    }
+
+    public Usuario getRanque() {
+        return ranque;
+    }
+
+    public void setRanque(Usuario ranque) {
+        this.ranque = ranque;
     }
     
 }
