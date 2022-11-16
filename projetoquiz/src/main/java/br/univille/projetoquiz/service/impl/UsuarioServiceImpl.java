9e268b5fd34623ac94462840567a19430e73b412
Binelo <jppedrobinelo@gmail.com>
2022-11-16 14:22:10 -0300

    quizeservice

---
+++ projetoquiz/src/main/java/br/univille/projetoquiz/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,17 @@
+package br.univille.projetoquiz.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import br.univille.projetoquiz.entity.Usuario;
+import br.univille.projetoquiz.service.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    @Override
+    public Usuario findById(long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
