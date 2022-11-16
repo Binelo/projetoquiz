12021897d22b1f7ae0b526372dd3cd98bd5d2131
Arthurlsk <arthurleonardo.strobel@gmail.com>
2022-11-16 15:15:14 -0300

    wip

---
+++ projetoquiz/src/main/java/br/univille/projetoquiz/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package br.univille.projetoquiz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+    
+
+    
+}
