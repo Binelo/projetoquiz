22fd1dde907400fdbf56147487fe7528d9cb657d
Binelo <jppedrobinelo@gmail.com>
2022-11-22 16:13:48 -0300

    rank

--- projetoquiz/src/main/java/br/univille/projetoquiz/controller/RankController.java
+++ projetoquiz/src/main/java/br/univille/projetoquiz/controller/RankController.java
@@ -1,12 +1,33 @@
 package br.univille.projetoquiz.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-public class RankController {
+import br.univille.projetoquiz.service.RankService;
 @Controller
 @RequestMapping("/rank")
-public class rankController {
+public class RankController {
+
+    @Autowired
+    private RankService service;
+
+    @GetMapping
+    public ModelAndView index() {
+        var listaRank = service.getAll();
+        return new ModelAndView("rank/index",
+                "listarank", listaRank);
     }
 
-}
+    // // @GetMapping("/novo")
+    // public ModelAndView novo() {
+    //     var novoRank = new Rank();
+    //     var listaUsuario = br.univille.projetoquiz.service.UsuarioService.getAll();
+    //     HashMap<String, Object> dados = new HashMap<>();
+    //     dados.put("Rank", novoRank);
+    //     dados.put("listaUsuario", listaUsuario);
+    //     dados.put("novoUsuario", new Usuario());
+    //     return new ModelAndView("Rank/form", dados);
+    }
